35d00904a4cca3d3c8a224e530a532eeeb30d7fd
AbdelrahmanHassan1 <abdelrahman.marei00@eng-st.cu.edu.eg>
2022-03-26 17:02:25 +0200

    adding code and design

--- expandable-card-animation/src/App.js
+++ expandable-card-animation/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { motion } from "framer-motion";
+import cards from "./cards";
+import "./App.css";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Expandable Cards</h1>
+      <motion.div
+        transition={{ layout: { duration: 0.6 } }}
+        layout
+        className="card"
+        onClick={() => setIsOpen(!isOpen)}
+        style={{
+          borderRadius: "1.5rem",
+          boxShadow: "0px 10px 30px rgba(0,0,0,0.5)",
+        }}
+      >
+        <motion.h2 layout="position">
+          {cards[0].name}
+          <span className="emoji" role="img">
+            {cards[0].emoji}
+          </span>
+        </motion.h2>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="expand_text"
+          >
+            <p>{cards[0].meaning}</p>
+          </motion.div>
+        )}
+      </motion.div>
     </div>
   );
 }
